d667d76b354cf8034f600edad26924282d535ac8
Abhinav <abhinav181@gmail.com>
2016-04-09 19:46:05 -0700

    second

--- data/yahoo/data.py
+++ data/yahoo/data.py
@@ -1,13 +1,23 @@
 import urllib2
 import datetime
+import pandas, StringIO
+import numpy as np
 
-
-def get_price(symbol, begin, end):
-    url = "http://real-chart.finance.yahoo.com/table.csv?s=AAPL&a=11&b=12&c=1980&d=00&e=4&f=2016&g=d&ignore=.csv"
+def get_price_csv(symbol, begin, end):
+    url = get_url(symbol, begin, end)
     response = urllib2.urlopen(url)
-    html = response.read()
-    print html
+    return response.read()
 
 def get_url(symbol, begin, end):
-    return "http://real-chart.finance.yahoo.com/table.csv?s=%s&a=%s&b=%s&c=1980&d=00&e=4&f=2016&g=d&ignore=.csv" % (
-        symbol, begin.month-1, begin,)
+    return "http://real-chart.finance.yahoo.com/table.csv?s=%s&a=%s&b=%s&c=%s&d=%s&e=%s&f=%s&g=d&ignore=.csv" % (
+        symbol, begin.month-1, begin.day, begin.year, end.month-1, end.day, end.year)
+
+end = datetime.date(2016, 4, 4);
+begin = datetime.date(2004, 4, 4);
+symbol = 'AAPL'
+prices = get_price_csv(symbol, begin, end)
+
+output = StringIO.StringIO()
+output.write(prices)
+output.seek(0)
+print pandas.read_csv(output, dtype={'Open': np.float64, 'Volume': np.float64})

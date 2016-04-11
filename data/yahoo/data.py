d48361bde4d3911da171352fd9f514a4ac484d48
Abhinav <abhinav181@gmail.com>
2016-04-10 19:11:55 -0700

    third

--- data/yahoo/data.py
+++ data/yahoo/data.py
@@ -3,10 +3,12 @@ import datetime
 import pandas, StringIO
 import numpy as np
 
+
 def get_price_csv(symbol, begin, end):
     url = get_url(symbol, begin, end)
     response = urllib2.urlopen(url)
     return response.read()
+
 
 def get_url(symbol, begin, end):
     return "http://real-chart.finance.yahoo.com/table.csv?s=%s&a=%s&b=%s&c=%s&d=%s&e=%s&f=%s&g=d&ignore=.csv" % (
@@ -17,7 +19,6 @@ begin = datetime.date(2004, 4, 4);
 symbol = 'AAPL'
 prices = get_price_csv(symbol, begin, end)
 
-output = StringIO.StringIO()
-output.write(prices)
-output.seek(0)
-print pandas.read_csv(output, dtype={'Open': np.float64, 'Volume': np.float64})
+print pandas.read_csv(StringIO.StringIO(prices),
+                      dtype={'Open': np.float64, 'Volume': np.float64},
+                      index_col='Date')
